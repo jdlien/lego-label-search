4e86730cb2d83f3d6a7cc35b5b8858948152e3c8
JD Lien <jd@jdlien.com>
2025-05-24 16:36:46 -0600

    cleanup, adding Toast component

--- components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './button'
-export { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './command'
-export { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './dialog'
-export { Popover, PopoverContent, PopoverTrigger } from './popover'
