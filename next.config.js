d6fec141ff80e69c2c28d98728d051ac538975a2
JD Lien <jd@jdlien.com>
2025-05-24 13:05:25 -0600

    Suppress libheif warning

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const nextConfig = {
   images: { unoptimized: true },
   */
   // Public directory files are automatically served by Next.js
+
+  webpack: (config, { isServer }) => {
+    // Suppress the warning for libheif-js dynamic require
+    config.ignoreWarnings = [
+      {
+        module: /libheif-js/,
+        message:
+          /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/,
+      },
+    ]
+
+    return config
+  },
 }
 
 module.exports = nextConfig
